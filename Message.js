26501475aaa2083d09d5ad37e2b72647a245a220
Zim Agu <zimmagu@gmail.com>
2021-01-18 11:37:29 -0800

    Added some stuff for the output

--- Message.js
+++ Message.js
@@ -15,13 +15,42 @@ import {pigLatin} from './MessageMixer.js';
 
 
 function displayMessage () {
+    console.log(`Count Characters:`);
     console.log(countCharacter("What is the color of the sky?", "t"));
+
+   console.log(``);
+
+    console.log(`Capitilize first character of words:`);
     console.log(capitalizeFirstCharacterOfWords("What is the color of the sky?"));
+
+    console.log(``);
+
+    console.log(`Reverse an entire sentence:`);
     console.log(reverseWord("What is the color of the sky?"));
+
+    console.log(``);
+
+    console.log(`Reverse every individual word in a sentence:`);
     console.log(reverseAllWords("What is the color of the sky?"));
+
+    console.log(``);
+    
+    console.log(`Replace one word with another word:`);
     console.log(replaceFirstOccurence("What is the color of the sky?", "sky", "water"));
+    
+    console.log(``);
+
+    console.log(`Encode a sentence:`);
     console.log(encode("What is the color of the sky?"));
+
+    console.log(``);
+
+    console.log(`palindrome:`);
     console.log(palindrome(`Reverse`));
+
+    console.log(``);
+
+    console.log(`pig latin:`);
     console.log(pigLatin(`What time is it?`, `z`));
   }
 
